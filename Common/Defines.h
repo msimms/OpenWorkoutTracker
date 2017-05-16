4f3679fd43a285330f968ea500e060aafbc0dd30
Mike Simms <msimms01@gmail.com>
2017-05-16 14:48:46 -0400

    Added MPL to new file.

--- Common/Defines.h
+++ Common/Defines.h
@@ -1,6 +1,10 @@
 // Created by Michael Simms on 5/16/17.
 // Copyright (c) 2017 Michael J. Simms. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #pragma once
 
 #define APP_NAME "Straen"
