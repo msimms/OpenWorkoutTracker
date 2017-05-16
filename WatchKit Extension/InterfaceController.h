78805488d8c643bf648af77b39378a11e989f54a
Mike Simms <msimms01@gmail.com>
2017-05-16 14:50:27 -0400

    Removed false start at creating a Watch app.

--- WatchKit Extension/InterfaceController.h
+++
@@ -1,9 +0,0 @@
-// Created by Michael Simms on 5/29/15.
-// Copyright (c) 2015 Michael J Simms Software. All rights reserved.
-
-#import <WatchKit/WatchKit.h>
-#import <Foundation/Foundation.h>
-
-@interface InterfaceController : WKInterfaceController
-
-@end
