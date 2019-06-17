f8d113e9c589db36e049074a9021463b1fde2bee
Mike Simms <msimms01@gmail.com>
2019-06-17 11:18:26 -0400

    Created an empty watch project.

---
+++ Watch Extension/ExtensionDelegate.h
@@ -0,0 +1,8 @@
+//  Created by Michael Simms on 6/12/19.
+//  Copyright © 2019 Michael J Simms Software. All rights reserved.
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ Watch Extension/InterfaceController.h
@@ -0,0 +1,9 @@
+//  Created by Michael Simms on 6/12/19.
+//  Copyright © 2019 Michael J Simms Software. All rights reserved.
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
