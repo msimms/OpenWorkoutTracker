46e71b3fa500bec5e1dcfa5803188df3eb7e992a
Mike Simms <msimms01@gmail.com>
2019-07-15 18:21:14 -0400

    Stubbed out a history view for the watch app.

---
+++ Watch Extension/WatchHistoryViewController.h
@@ -0,0 +1,11 @@
+//  Created by Michael Simms on 7/15/19.
+//  Copyright © 2019 Michael J Simms Software. All rights reserved.
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface WatchHistoryViewController : WKInterfaceController
+{
+}
+
+@end
---
+++ Watch Extension/WatchHistoryViewController.m
@@ -0,0 +1,38 @@
+//  Created by Michael Simms on 7/15/19.
+//  Copyright © 2019 Michael J Simms Software. All rights reserved.
+
+#import "WatchHistoryViewController.h"
+#import "ActivityMgr.h"
+#import "ExtensionDelegate.h"
+
+@interface WatchHistoryViewController ()
+
+@end
+
+
+@implementation WatchHistoryViewController
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self)
+	{
+	}
+	return self;
+}
+
+- (void)willActivate
+{
+	[super willActivate];
+}
+
+- (void)didDeactivate
+{
+	[super didDeactivate];
+}
+
+- (void)didAppear
+{
+}
+
+@end
