0d06c59114713d7543c658b8cad4d06531dad87b
Mike Simms <msimms01@gmail.com>
2019-09-16 11:57:08 -0400

    Added license to the watch app source files.

--- Watch Extension/InterfaceController.m
+++ Watch Extension/InterfaceController.m
@@ -1,6 +1,10 @@
 //  Created by Michael Simms on 6/12/19.
 //  Copyright © 2019 Michael J Simms Software. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #import "InterfaceController.h"
 #import "ActivityMgr.h"
 #import "AppStrings.h"
--- Watch Extension/WatchSessionManager.h
+++ Watch Extension/WatchSessionManager.h
@@ -1,6 +1,10 @@
 //  Created by Michael Simms on 7/28/19.
 //  Copyright © 2019 Michael J Simms Software. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #import <WatchKit/WatchKit.h>
 #import <WatchConnectivity/WatchConnectivity.h>
 
