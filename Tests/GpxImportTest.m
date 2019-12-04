544d4bf0d9a606ebebf6d91e9e4204c901103f1d
Mike Simms <msimms01@gmail.com>
2019-12-04 14:30:40 -0500

    Updated copyright on unit test classes.

--- Tests/GpxImportTest.m
+++ Tests/GpxImportTest.m
@@ -1,6 +1,10 @@
 // Created by Michael Simms on 4/4/13.
 // Copyright (c) 2013 Michael J. Simms. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #import <XCTest/XCTest.h>
 #import "ActivityMgr.h"
 #import "ActivityType.h"
--- Tests/Tests.m
+++ Tests/Tests.m
@@ -1,10 +1,9 @@
-//
-//  Tests.m
-//  Tests
-//
-//  Created by Michael Simms on 11/29/19.
-//  Copyright © 2019 MJ Simms Software LLC. All rights reserved.
-//
+// Created by Michael Simms on 11/29/19.
+// Copyright (c) 2019 Michael J. Simms. All rights reserved.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #import <XCTest/XCTest.h>
 
