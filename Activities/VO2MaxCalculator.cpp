fe5215137fcd7452a9a676d710e8a767e924950a
Mike Simms <msimms01@gmail.com>
2019-12-10 14:43:54 -0500

    Updated copyright.

--- Activities/VO2MaxCalculator.cpp
+++ Activities/VO2MaxCalculator.cpp
@@ -1,6 +1,10 @@
 // Created by Michael Simms on 12/10/19.
 // Copyright (c) 2019 Michael J. Simms. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include "VO2MaxCalculator.h"
 
 VO2MaxCalculator::VO2MaxCalculator()
