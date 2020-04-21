543837844a5b254aaa3e4290b1ef7e6500866a0a
Mike Simms <msimms01@gmail.com>
2020-04-21 13:18:05 -0400

    Updated copyright.

--- IOS/View/ShoeProfileViewController.h
+++ IOS/View/ShoeProfileViewController.h
@@ -1,6 +1,10 @@
 // Created by Michael Simms on 4/19/20.
 // Copyright (c) 2020 Michael J. Simms. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #import "CommonViewController.h"
 
 typedef enum ShoeProfileViewMode
