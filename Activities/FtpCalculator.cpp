b6f67f1976ba55cf9ec268297b575dc6c21aca62
Mike Simms <msimms01@gmail.com>
2020-08-01 13:46:48 -0400

    Updated license for the FTP calculator.

--- Activities/FtpCalculator.cpp
+++ Activities/FtpCalculator.cpp
@@ -1,6 +1,10 @@
 // Created by Michael Simms on 7/20/20.
 // Copyright (c) 2020 Michael J. Simms. All rights reserved.
 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include "FtpCalculator.h"
 
 FtpCalculator::FtpCalculator()
