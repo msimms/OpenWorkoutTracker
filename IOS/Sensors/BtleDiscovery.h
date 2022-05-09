d82cac52b74925372a0690ee0eda1538a614a5d1
Mike Simms <msimms01@gmail.com>
2022-05-09 18:09:58 -0400

    Removed dead code.

--- IOS/Sensors/BtleDiscovery.h
+++ IOS/Sensors/BtleDiscovery.h
@@ -81,26 +81,6 @@
 
 - (void)allowConnectionsFromUnknownDevices:(BOOL)allow;
 
-- (void)connectPeripheral:(CBPeripheral*)peripheral;
-- (void)disconnectPeripheral:(CBPeripheral*)peripheral;
-
-- (void)centralManager:(CBCentralManager*)central didConnectPeripheral:(CBPeripheral*)peripheral;
-- (void)centralManager:(CBCentralManager*)central didDisconnectPeripheral:(CBPeripheral*)peripheral error:(NSError*)error;
-- (void)centralManager:(CBCentralManager*)central didDiscoverPeripheral:(CBPeripheral*)peripheral advertisementData:(NSDictionary*)advertisementData RSSI:(NSNumber*)RSSI;
-- (void)centralManager:(CBCentralManager*)central didFailToConnectPeripheral:(CBPeripheral*)peripheral error:(NSError*)error;
-- (void)centralManager:(CBCentralManager*)central didRetrieveConnectedPeripherals:(NSArray*)peripherals;
-- (void)centralManager:(CBCentralManager*)central didRetrievePeripherals:(NSArray*)peripherals;
-- (void)centralManagerDidUpdateState:(CBCentralManager*)central;
-
-- (void)peripheral:(CBPeripheral*)peripheral didDiscoverServices:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didDiscoverIncludedServicesForService:(CBService *)service error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didDiscoverCharacteristicsForService:(CBService*)service error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didUpdateValueForCharacteristic:(CBCharacteristic*)characteristic error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didUpdateValueForDescriptor:(CBDescriptor*)descriptor error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didWriteValueForCharacteristic:(CBCharacteristic*)characteristic error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didWriteValueForDescriptor:(CBDescriptor*)descriptor error:(NSError*)error;
-- (void)peripheral:(CBPeripheral*)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic*)characteristic error:(NSError*)error;
-
 - (NSMutableArray*)discoveredPeripheralsWithServiceId:(BluetoothServiceId)serviceId;
 - (NSMutableArray*)discoveredPeripheralsWithCustomServiceId:(NSString*)serviceId;
 - (NSMutableArray*)sensorsForPeripheral:(CBPeripheral*)peripheral;
