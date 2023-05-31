4c42b61c5169208026d0d9aaa794079a05d2296e
Mike Simms <msimms01@gmail.com>
2023-05-31 12:44:06 -0400

    Updated the VO2Max calculation

--- Activities/VO2MaxCalculator.cpp
+++ Activities/VO2MaxCalculator.cpp
@@ -6,6 +6,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "VO2MaxCalculator.h"
+#include <math.h>
 
 VO2MaxCalculator::VO2MaxCalculator()
 {
@@ -20,10 +21,12 @@ double VO2MaxCalculator::EstimateVO2MaxFromHeartRate(double maxHR, double restin
 	return 15.3 * (maxHR / restingHR);
 }
 
+/// @brief "Daniels and Gilbert VO2 Max formula
 double VO2MaxCalculator::EstimateVO2MaxFromRaceDistanceInMeters(double raceDistanceMeters, double raceTimeMinutes)
 {
-	double speed = raceDistanceMeters / raceTimeMinutes;
-	return -4.60 + 0.182258 * speed + 0.000104 * speed * speed;
+	double t = raceTimeMinutes;
+	double v = raceDistanceMeters / t;
+	return (-4.60 + 0.182258 * v + 0.000104 * pow(v, 2.0)) / (0.8 + 0.1894393 * pow(exp(1), -0.012778 * t) + 0.2989558 * pow(exp(1), -0.1932605 * t));
 }
 
 double VO2MaxCalculator::EstimateVO2MaxFromRaceDistanceInMetersAndHeartRate(double raceDistanceMeters, double raceTimeMinutes, double loadHr, double restingHr, double maxHr)
