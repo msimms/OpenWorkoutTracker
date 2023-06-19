d2fd4f25d96baf29b50da5fd11719f742d9497f9
Mike Simms <msimms01@gmail.com>
2023-06-19 14:06:26 -0400

    More line chart fixes.

--- iPhone App/LineGraphView.swift
+++ iPhone App/LineGraphView.swift
@@ -86,6 +86,7 @@ struct LineGraphView: View {
 	let points: Array<LinePoint>
 	let color: Color
 	let formatter: ((_ num: Double) -> String)?
+	@State private var orientation = UIDevice.current.orientation
 
 	init(points: [(UInt64, Double)], color: Color, formatter: ((_ num: Double) -> String)?) {
 		self.points = points.map { LinePoint(x:$0, y:$1) }
@@ -104,8 +105,8 @@ struct LineGraphView: View {
 		GeometryReader { geometry in
 			let canvasMinX: Double = 50.0
 			let canvasMinY: Double = 10.0
-			let numXHashmarks: Int = 10
-			let numYHashmarks: Int = 10
+			let numXHashmarks: Int = self.orientation.isLandscape ? 10 : 5
+			let numYHashmarks: Int = self.orientation.isLandscape ? 5 : 10
 			let hashMarkLength: Double = 16.0
 			let axisWidth: Double = 4.0
 			let canvasMaxX: Double = geometry.size.width - canvasMinX
@@ -115,6 +116,8 @@ struct LineGraphView: View {
 			let yAxisTop: CGPoint = CGPoint(x: origin.x, y: canvasMinY - axisWidth)
 			var axisXOffset: Double = canvasMaxX
 			var axisYOffset: Double = canvasMaxY
+			let xAxisHashMarkSpacing = (canvasMaxX / Double(numXHashmarks))
+			let yAxisHashMarkSpacing = ((canvasMaxY - canvasMinY) / Double(numYHashmarks))
 			let components = UIColor(self.color).cgColor.components!
 			let fadedColor = Color(red: components[0] * 0.5, green: components[1] * 0.5, blue: components[2] * 0.5)
 			let gradient = LinearGradient(
@@ -142,7 +145,7 @@ struct LineGraphView: View {
 				Path { path in
 					for _ in 1...numXHashmarks {
 						let canvasX = canvasMinX + (canvasMaxX - axisXOffset)
-						axisXOffset -= (canvasMaxX / Double(numXHashmarks))
+						axisXOffset -= xAxisHashMarkSpacing
 						
 						path.move(to: CGPoint(x: canvasX, y: canvasMaxY))
 						path.addLine(to: CGPoint(x: canvasX, y: canvasMaxY + hashMarkLength))
@@ -154,7 +157,12 @@ struct LineGraphView: View {
 				Path { path in
 					for _ in 1...numYHashmarks {
 						let canvasY = canvasMinY + (canvasMaxY - axisYOffset)
-						axisYOffset -= (canvasMaxY / Double(numYHashmarks))
+						axisYOffset -= yAxisHashMarkSpacing
+						
+						// Don't draw past the axis line.
+						if axisYOffset <= yAxisTop.y {
+							break
+						}
 
 						path.move(to: CGPoint(x: canvasMinX, y: canvasY))
 						path.addLine(to: CGPoint(x: canvasMinX - hashMarkLength, y: canvasY))
@@ -163,9 +171,13 @@ struct LineGraphView: View {
 				.stroke(.gray, lineWidth: axisWidth / 2)
 
 				// Draw the data line.
-				Line(points: self.points, origin: origin)
-					.fill(gradient)
-					.frame(width: xAxisTop.x - origin.x, height: origin.y - yAxisTop.y)
+				let lineWidth = xAxisTop.x - origin.x
+				let lineHeight = origin.y - yAxisTop.y
+				if lineWidth > 0 && lineHeight > 0 {
+					Line(points: self.points, origin: origin)
+						.fill(gradient)
+						.frame(width: lineWidth, height: lineHeight)
+				}
 			}
 
 			Group() {
@@ -175,16 +187,22 @@ struct LineGraphView: View {
 				let maxY = self.points.map { $0.y }.max() ?? 0
 				let rangeY = maxY - minY
 				ForEach(1..<11) { i in
-					let canvasYOffset: Double = Double(i) * (canvasMaxY / Double(numYHashmarks))
+					let canvasYOffset: Double = Double(i) * yAxisHashMarkSpacing
 					let canvasY: Double = canvasMinY + (canvasMaxY - canvasYOffset)
 					let axisStep: Double = Double(i) * (rangeY / Double(numYHashmarks))
 					let axisValue: Double = minY + axisStep
 					let formattedValue: String = self.formatYAxisValue(num: axisValue)
 
-					Text(formattedValue)
-						.position(x: origin.x - 30.0, y: canvasY - 12.0)
+					// Don't draw past the axis line.
+					if canvasY > yAxisTop.y {
+						Text(formattedValue)
+							.position(x: origin.x - 28.0, y: canvasY - 20.0)
+					}
 				}
 			}
+		}
+		.onRotate { newOrientation in
+			self.orientation = newOrientation
 		}
     }
 }
