6a39d5e0029dd8b323e29d614d67fb4e829f10ce
Mike Simms <msimms01@gmail.com>
2024-02-21 12:46:51 -0500

    Added the ability to copy debug info for workout generation, useful for debugging.

--- iPhone App/DebugView.swift
+++ iPhone App/DebugView.swift
@@ -6,16 +6,36 @@
 import SwiftUI
 
 struct DebugView: View {
+	@Environment(\.colorScheme) var colorScheme
 	var inputs: Dictionary<String, Any> = [:]
 
 	var body: some View {
-		List(self.inputs.keys.sorted(), id: \.self) { item in
-			VStack(alignment: .leading) {
-				Text(item)
-					.bold()
-				let val = self.inputs[item] as? Double
-				Text(String(val!))
+		VStack(alignment: .center) {
+			List(self.inputs.keys.sorted(), id: \.self) { item in
+				VStack(alignment: .leading) {
+					Text(item)
+						.bold()
+					let val = self.inputs[item] as? Double
+					Text(String(val!))
+				}
 			}
 		}
+
+		VStack(alignment: .center) {
+			Button {
+				let pasteboard = UIPasteboard.general
+				pasteboard.string = self.inputs.description
+			} label: {
+				Text("Copy")
+					.foregroundColor(self.colorScheme == .dark ? .black : .white)
+					.fontWeight(Font.Weight.heavy)
+					.frame(minWidth: 0, maxWidth: .infinity)
+					.padding()
+			}
+			.background(RoundedRectangle(cornerRadius: 10, style: .continuous))
+			.opacity(0.8)
+			.bold()
+		}
+		.padding(10)
     }
 }
